36eec3f3f866288eb4153d9068158d8599abfe8a
Student246752 <246752@student.pwr.edu.pl>
2021-04-21 04:09:10 +0200

    Add game launch

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-
+import GameEngine
+from itertools import cycle
+from GameEngine import Game
 
 
 def print_hi(name):
     print(f'Hi, {name}')
 
-if __name__ == '__main__':
-    print_hi('Konrad Liuras')
 
+if __name__ == '__main__':
+    game = Game()
+    game.start_game()
