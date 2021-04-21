e3e85b13335aecf5bce17e4ef16bdba637027a53
Student246752 <246752@student.pwr.edu.pl>
2021-04-21 04:09:47 +0200

    Add types and str functions

--- Components.py
+++ Components.py
@@ -1,18 +1,35 @@
+from typing import List
+
+from Player import Player
+
+
 class Stone:
     def __init__(self, color):
         self.color = color
 
+    def __str__(self):
+        return str(self.color)
+
 
 class Hole:
     def __init__(self, number, stones, player):
-        self.stones = stones
-        self.number = number
-        self.player = player
+        self.stones: List[Stone] = stones
+        self.number: int = number
+        self.player: Player = player
+
+    def add_stone(self, stone):
+        self.stones.append(stone)
+
+    def __str__(self):
+        return str([str(stone) for stone in self.stones])
 
 
 class Mancala(Hole):
     def __init__(self, stones, player):
-        super().__init__(0, stones, player)
+        super().__init__(7, stones, player)
 
     def points(self):
         return len(self.stones)
+
+    def __str__(self):
+        return f"[[ {str([str(stone) for stone in self.stones])} ]]"
