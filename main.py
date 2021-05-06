c76702c5e0de6173454eefdf91b88fc814e60d24
Student246752 <246752@student.pwr.edu.pl>
2021-05-07 00:48:20 +0200

    Testing loop

--- main.py
+++ main.py
@@ -11,12 +11,16 @@ def print_hi(name):
 
 
 if __name__ == '__main__':
-    game = Game()
+
 
     # profile = cProfile.Profile()
     # profile.runcall(lambda: game.start_game())
     # ps = pstats.Stats(profile)
     # ps.print_stats()
 
-    game.start_game()
+    for level in range(4, 5):
+        for mode in ["minmax", "alfabeta"]:
+            for i in range(1, 7):
+                game = Game()
+                game.start_game(first_move=i, testing=True, ai_depth_level=level, mode=mode)
 
