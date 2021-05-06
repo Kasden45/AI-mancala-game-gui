232cb0e0e44c6a2520bee584a4d16abb84c9a338
Student246752 <246752@student.pwr.edu.pl>
2021-05-07 00:49:43 +0200

    Add AI mode and AI's computing time attributes

--- Player.py
+++ Player.py
@@ -4,6 +4,8 @@ class Player:
         self.points = 0
         self.name = name
         self.type = "Human"  # AI
+        self.AI_mode = "minmax"
+        self.computing_time = 0
 
     def __str__(self):
-        return f"Id: {self.id} -- Name: {self.name} -- Points: {self.points}"
+        return f"Id: {self.id} -- Name: {self.name} -- Points: {self.points} {self.AI_mode if self.type == 'AI' else ''}"
